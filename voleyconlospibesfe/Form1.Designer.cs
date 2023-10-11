7baab139231786bafca985a916dc336d64a8e060
delga <delga@LAPTOP-8B5KT0RE>
2023-10-11 06:42:45 -0300

    Agregar archivos de proyecto.

---
+++ voleyconlospibesfe/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace voleyconlospibesfe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblApellidos = new System.Windows.Forms.Label();
+            this.txtApellidos = new System.Windows.Forms.TextBox();
+            this.lblNumeros = new System.Windows.Forms.Label();
+            this.txtNumeros = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.lblSaludar = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(5, 40);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(205, 23);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre ";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(12, 66);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(200, 28);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // lblApellidos
+            // 
+            this.lblApellidos.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblApellidos.Location = new System.Drawing.Point(5, 97);
+            this.lblApellidos.Name = "lblApellidos";
+            this.lblApellidos.Size = new System.Drawing.Size(212, 23);
+            this.lblApellidos.TabIndex = 2;
+            this.lblApellidos.Text = "Apellidos";
+            // 
+            // txtApellidos
+            // 
+            this.txtApellidos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtApellidos.Location = new System.Drawing.Point(10, 123);
+            this.txtApellidos.Name = "txtApellidos";
+            this.txtApellidos.Size = new System.Drawing.Size(200, 26);
+            this.txtApellidos.TabIndex = 3;
+            // 
+            // lblNumeros
+            // 
+            this.lblNumeros.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumeros.Location = new System.Drawing.Point(5, 152);
+            this.lblNumeros.Name = "lblNumeros";
+            this.lblNumeros.Size = new System.Drawing.Size(218, 23);
+            this.lblNumeros.TabIndex = 4;
+            this.lblNumeros.Text = "Numero de camiseta";
+            // 
+            // txtNumeros
+            // 
+            this.txtNumeros.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumeros.Location = new System.Drawing.Point(10, 178);
+            this.txtNumeros.Name = "txtNumeros";
+            this.txtNumeros.Size = new System.Drawing.Size(200, 26);
+            this.txtNumeros.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::voleyconlospibesfe.Properties.Resources.canchita_de_voley;
+            this.pictureBox1.Location = new System.Drawing.Point(216, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(570, 317);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCargar.Location = new System.Drawing.Point(59, 248);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(83, 52);
+            this.btnCargar.TabIndex = 7;
+            this.btnCargar.Text = "Cargar jugador";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblSaludar
+            // 
+            this.lblSaludar.Location = new System.Drawing.Point(435, 123);
+            this.lblSaludar.Name = "lblSaludar";
+            this.lblSaludar.Size = new System.Drawing.Size(135, 25);
+            this.lblSaludar.TabIndex = 8;
+            this.lblSaludar.Text = " .";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 391);
+            this.Controls.Add(this.lblSaludar);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtNumeros);
+            this.Controls.Add(this.lblNumeros);
+            this.Controls.Add(this.txtApellidos);
+            this.Controls.Add(this.lblApellidos);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblApellidos;
+        private System.Windows.Forms.TextBox txtApellidos;
+        private System.Windows.Forms.Label lblNumeros;
+        private System.Windows.Forms.TextBox txtNumeros;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.Label lblSaludar;
+    }
+}
+
---
+++ voleyconlospibesfe/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace voleyconlospibesfe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.lblSaludar.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.lblSaludar.Text = this.txtNombre.Text + " " + this.txtApellidos.Text + " " + this.txtNumeros.Text;
+
+            
+        }
+
+        private void Jugador_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+    }
+
